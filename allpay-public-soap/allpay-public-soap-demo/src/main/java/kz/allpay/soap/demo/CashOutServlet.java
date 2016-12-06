c4944aa98b06ef248a0c0199eb52b4872a3b56c6
Sanzhar <sanzhar.aubakirov@innoforce.kz>
2016-12-06 16:28:02 +0600

    Start writing demo. Add bootstrap

---
+++ allpay-public-soap/allpay-public-soap-demo/src/main/java/kz/allpay/soap/demo/CashOutServlet.java
@@ -0,0 +1,8 @@
+package kz.allpay.soap.demo;
+
+/**
+ * User: Sanzhar Aubakirov
+ * Date: 12/6/16
+ */
+public class CashOutServlet {
+}
---
+++ allpay-public-soap/allpay-public-soap-demo/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<html>
+<body>
+<h2>Hello World!</h2>
+</body>
+</html>
