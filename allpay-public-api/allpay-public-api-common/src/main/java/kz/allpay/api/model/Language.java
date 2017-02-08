949fd130a3bbf8f14b3a5ed9b8a2b6f872801793
Sanzhar <sanzhar.aubakirov@innoforce.kz>
2017-02-08 16:56:57 +0600

    Configure swagger annotations

--- allpay-public-api/allpay-public-api-common/src/main/java/kz/allpay/api/model/Language.java
+++ allpay-public-api/allpay-public-api-common/src/main/java/kz/allpay/api/model/Language.java
@@ -1,9 +1,12 @@
 package kz.allpay.api.model;
 
+import io.swagger.annotations.ApiModel;
+
 /**
  * User: Sanzhar Aubakirov
  * Date: 6/30/16
  */
+@ApiModel
 public enum Language {
     EN, RU, KK;
 }
