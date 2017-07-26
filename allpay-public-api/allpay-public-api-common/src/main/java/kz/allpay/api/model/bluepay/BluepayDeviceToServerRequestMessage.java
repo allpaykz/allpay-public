51c815da4e770767738a68c636c20e64df76e9c1
magzhan.karasayev <fallen.s4e@gmail.com>
2017-07-26 20:02:26 +0600

    Upd: bluepay message updated, signature and keyadded

--- allpay-public-api/allpay-public-api-common/src/main/java/kz/allpay/api/model/bluepay/BluepayDeviceToServerRequestMessage.java
+++ allpay-public-api/allpay-public-api-common/src/main/java/kz/allpay/api/model/bluepay/BluepayDeviceToServerRequestMessage.java
@@ -10,12 +10,16 @@ public class BluepayDeviceToServerRequestMessage {
     private final BigDecimal amount;
     private final String deviceId;
     private final String mobileAppMessage;
+    private final String devicePublicKeyBase64;
+    private final String devicePublicKeySignature;
     private String base64Signature;
 
-    public BluepayDeviceToServerRequestMessage(BigDecimal amount, String deviceId, String mobileAppMessage) {
+    public BluepayDeviceToServerRequestMessage(BigDecimal amount, String deviceId, String mobileAppMessage, String devicePublicKeyBase64, String devicePublicKeySignature) {
         this.amount = amount;
         this.deviceId = deviceId;
         this.mobileAppMessage = mobileAppMessage;
+        this.devicePublicKeyBase64 = devicePublicKeyBase64;
+        this.devicePublicKeySignature = devicePublicKeySignature;
     }
 
     public BigDecimal getAmount() {
@@ -34,11 +38,19 @@ public class BluepayDeviceToServerRequestMessage {
         return base64Signature;
     }
 
-    public String getDataToSign() {
-        return amount + deviceId + mobileAppMessage;
-    }
-
     public void setBase64Signature(String base64Signature) {
         this.base64Signature = base64Signature;
+    }
+
+    public String getDevicePublicKeyBase64() {
+        return devicePublicKeyBase64;
+    }
+
+    public String getDevicePublicKeySignature() {
+        return devicePublicKeySignature;
+    }
+
+    public String getMessageToSign() {
+        return new StringBuilder().append(amount).append(deviceId).append(mobileAppMessage).toString();
     }
 }
