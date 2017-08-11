02d2141b537643d6464f58c3146603bfb4e2f7a3
magzhan.karasayev <fallen.s4e@gmail.com>
2017-08-11 12:15:32 +0600

    Upd: another parameter added to BluePayResponse

--- allpay-public-api/allpay-public-api-common/src/main/java/kz/allpay/api/model/bluepay/BluePayTxResponse.java
+++ allpay-public-api/allpay-public-api-common/src/main/java/kz/allpay/api/model/bluepay/BluePayTxResponse.java
@@ -6,16 +6,21 @@ import java.io.Serializable;
 import kz.allpay.api.model.response.AbstractResponse;
 
 /**
- * Contains nothing
  * @author magzhan.karasayev
  * @since 11/16/16 7:13 PM
  */
 @ApiModel
 public class BluePayTxResponse implements Serializable {
-    private String m;
+    private String m; // error code
+    private String c; // comment
 
     public BluePayTxResponse(String m) {
         this.m = m;
+    }
+
+    public BluePayTxResponse(String m, String c) {
+        this.m = m;
+        this.c = c;
     }
 
     public BluePayTxResponse() {
@@ -29,5 +34,14 @@ public class BluePayTxResponse implements Serializable {
     public BluePayTxResponse setM(String m) {
         this.m = m;
         return this;
+    }
+
+    @ApiModelProperty(notes = "Опциональный комментарий(зарезервировано)")
+    public String getC() {
+        return c;
+    }
+
+    public void setC(String c) {
+        this.c = c;
     }
 }
